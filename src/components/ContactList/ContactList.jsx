b4b8cc1c51c86af2bd251f0f256e6bfe5f827ee4
Anna Kovalchuk <annazukh@gmail.com>
2023-01-07 23:55:09 +0200

    select visible

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,23 @@
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { List } from './ContactList.styled';
 import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filterValue = useSelector(getFilter);
+  const contacts = useSelector(selectVisibleContacts);
 
   return (
     <List>
-      {contacts
-        .filter(contact =>
-          contact.name.toLowerCase().includes(filterValue.toLowerCase())
-        )
-        .map(contact => {
-          return (
-            <ContactListItem
-              key={contact.id}
-              id={contact.id}
-              name={contact.name}
-              phone={contact.phone}
-            />
-          );
-        })}
+      {contacts.map(contact => {
+        return (
+          <ContactListItem
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            phone={contact.phone}
+          />
+        );
+      })}
     </List>
   );
 };
