219b291c67494a11fed872b77b18c2e07b61653c
Anna Kovalchuk <annazukh@gmail.com>
2023-01-07 23:13:39 +0200

    add_indicators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { RootEl } from './App.styled';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
+import { selectIsLoading, selectErrors } from '../redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectErrors);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -17,9 +20,9 @@ export const App = () => {
     <RootEl>
       <h1>Phonebook</h1>
       <ContactForm />
-
       <h2>Contacts</h2>
       <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList />
     </RootEl>
   );
